bf84e074eb3e1c6251d2abc30a0812a7afeea02f
seongjun yoo <edonc700@gmail.com>
2022-02-22 00:24:28 +0900

    Update Solution.java

--- Prgms/고득점 Kit/힙(Heap)/이중우선순위/Solution.java
+++ Prgms/고득점 Kit/힙(Heap)/이중우선순위/Solution.java
@@ -1,45 +1,102 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 class Solution {
-    public int solution(int[][] jobs) {
-        PriorityQueue<int[]> nextJobQ = new PriorityQueue<>(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				// TODO Auto-generated method stub
-				if(o1[0] == o2[0]) return o1[1] - o2[1];
-				return o1[0] - o2[0];
+    public int[] solution(String[] operations) {
+        int[] answer = {};
+        PriorityDeque pdq = new PriorityDeque();
+        StringTokenizer st;
+        for (int i = 0; i < operations.length; i++) {
+			st = new StringTokenizer(operations[i]);
+			char command = st.nextToken().charAt(0);
+			int val = Integer.parseInt(st.nextToken());
+			if(command == 'I') pdq.offer(val);
+			else {
+				if(val == 1 && !pdq.isEmpty()) pdq.removeMax();
+				else if(val == -1 && !pdq.isEmpty()) pdq.removeMin();
 			}
-		});
-        
-        PriorityQueue<int[]> waitingQ = new PriorityQueue<>(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				// TODO Auto-generated method stub
-				return o1[1] - o2[1];
-			}
-		});
-        
-        for (int i = 0; i < jobs.length; i++) {
-			nextJobQ.offer(jobs[i]);
 		}
         
-        int currentTime = 0;
-        int totalJobTime = 0;
-        while(!nextJobQ.isEmpty() || !waitingQ.isEmpty()) {
-        	if(waitingQ.isEmpty()) {
-        		int[] currentJob = nextJobQ.poll();
-        		currentTime = currentJob[0] + currentJob[1];
-        		totalJobTime += currentJob[1];
-        	} else {
-        		int[] currentJob = waitingQ.poll();
-        		currentTime += currentJob[1];
-        		totalJobTime += currentTime - currentJob[0];
-        	}
-        	
-        	while(!nextJobQ.isEmpty() && nextJobQ.peek()[0] <= currentTime) waitingQ.offer(nextJobQ.poll());
-        }
-        
-        return totalJobTime / jobs.length;
+        if(pdq.isEmpty()) answer = new int[] {0, 0};
+        else answer = new int[] {pdq.tail.value, pdq.head.value};
+        return answer;
     }
+}
+
+
+class PriorityDeque {
+	Node head;
+	Node tail;
+	int size;
+	
+	public PriorityDeque() {
+		super();
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
+	}
+	
+	public void offer(int value) {
+		Node currentNode = this.head;
+		boolean checkLast = true;
+		while(currentNode != null) {
+			if(currentNode.value >= value) {
+				Node prevNode = currentNode.prev;
+				Node newNode = new Node(currentNode, prevNode, value);
+				currentNode.prev = newNode;
+				if(prevNode == null) this.head = newNode;
+				else prevNode.next = newNode;
+				checkLast = false;
+			}
+			currentNode = currentNode.next;
+		}
+		
+		if(checkLast) {
+			Node prevNode = this.tail;
+			Node newNode = new Node(null, prevNode, value);
+			this.tail = newNode;
+			if(this.head == null) this.head = newNode;
+			if(prevNode != null) prevNode.next = newNode;
+		}
+		
+		this.size++;
+	}
+	
+	public void removeMin() {
+		if(this.size == 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.head = this.head.next;
+			this.head.prev = null;
+		}
+		this.size--;
+	}
+	
+	public void removeMax() {
+		if(this.size == 1) {
+			this.tail = null;
+			this.head = null;
+		} else {
+			this.tail = this.tail.prev;
+			this.tail.next = null;
+		}
+		this.size--;
+	}
+	
+	public boolean isEmpty() {
+		return this.size == 0 ? true : false;
+	}
+}
+
+class Node {
+	Node next;
+	Node prev;
+	int value;
+	
+	public Node(Node next, Node prev, int value) {
+		super();
+		this.next = next;
+		this.prev = prev;
+		this.value = value;
+	}
 }

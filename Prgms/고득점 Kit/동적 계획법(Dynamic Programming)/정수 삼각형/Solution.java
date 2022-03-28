615722f0c306c8e4380959254318f07b155911ba
seongjun yoo <edonc700@gmail.com>
2022-03-28 18:03:22 +0900

    Add files via upload

--- Prgms/고득점 Kit/동적 계획법(Dynamic Programming)/정수 삼각형/Solution.java
+++ Prgms/고득점 Kit/동적 계획법(Dynamic Programming)/정수 삼각형/Solution.java
@@ -1,26 +1,48 @@
+import java.util.HashSet;
+
 class Solution {
-    public int solution(int[][] triangle) {
-        int[][] sum = new int[triangle.length][];
-        for (int i = 0; i < triangle.length; i++) {
-			sum[i] = new int[i + 1];
+    public int solution(int N, int number) {
+        int answer = -1;
+        if(N == number) return 1;
+        HashSet<Integer>[] dp = new HashSet[9];
+        for (int i = 1; i < dp.length; i++) {
+			dp[i] = new HashSet<Integer>();
+			String s = "";
+			for (int j = 0; j < i; j++) {
+				s += N;
+			}
+			dp[i].add(Integer.parseInt(s));
 		}
         
-        sum[0][0] = triangle[0][0];
-        for (int i = 1; i < triangle.length; i++) {
-			int len = i + 1;
-			for (int j = 0; j < len; j++) {
-				int parent1 = j - 1 < 0 ? -1 : sum[i - 1][j - 1];
-				int parent2 = j == len - 1 ? -1 : sum[i - 1][j];
-				
-				sum[i][j] = triangle[i][j] + Math.max(parent1, parent2);
+        for (int i = 2; i < dp.length; i++) {
+			makeDp(dp, i);
+			if(dp[i].contains(number)) {
+				answer = i;
+				break;
 			}
 		}
-        
-        int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < triangle.length; i++) {
-			answer = Math.max(answer, sum[triangle.length - 1][i]);
-		}
-        
         return answer;
     }
+
+	private void makeDp(HashSet<Integer>[] dp, int len) {
+		int left = 0;
+		int right = len;
+		
+		for (int i = 0; i < len - 1; i++) {
+			left += 1;
+			right -= 1;
+			
+			HashSet<Integer> dpLeft = dp[left];
+			HashSet<Integer> dpRight = dp[right];
+			
+			for (Integer integer : dpLeft) {
+				for (Integer integer2 : dpRight) {
+					if(integer + integer2 != 0)dp[len].add(integer + integer2);
+					if(integer - integer2 != 0)dp[len].add(integer - integer2);
+					if(integer / integer2 != 0)dp[len].add(integer / integer2);
+					dp[len].add(integer * integer2);
+				}
+			}
+		}
+	}
 }
